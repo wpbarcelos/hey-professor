c132a01fff13853cbe3c26b60f4d12cf27af8134
Wanderson P. Barcelos <wp.barcelos@gmail.com>
2024-12-12 08:25:43 -0300

    installing larastan, setting up and fixing all errors

--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -2,14 +2,16 @@
 
 namespace App\View\Components;
 
-use Illuminate\View\{Component, View};
+use Illuminate\View\{Component};
+use Illuminate\Contracts\View\View;
 
 class GuestLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
+     *  @return \Illuminate\Contracts\View\View
      */
-    public function render(): View
+    public function render()
     {
         return view('layouts.guest');
     }
